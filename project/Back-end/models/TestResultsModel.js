0aed3732a3bd4e10fbeab42cb2bb376ad09c1a9a
khushalibhatt <91697920+khushalibhatt@users.noreply.github.com>
2024-12-10 22:00:52 -0500

    <save test results><1>: <added test results models.js, a new model for test results earned by the students. changed so we can directly save a students test results>

---
+++ project/Back-end/models/TestResultsModel.js
@@ -0,0 +1,50 @@
+import { Sequelize, DataTypes } from "sequelize";
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "database.sqlite",
+});
+
+const TestResult = sequelize.define("TestResult", {
+    userId: { 
+        type: DataTypes.INTEGER, 
+        allowNull: false 
+    },
+    setName: { 
+        type: DataTypes.STRING, 
+        allowNull: false 
+    },
+    score: { 
+        type: DataTypes.FLOAT, 
+        allowNull: false 
+    },
+    totalQuestions: { 
+        type: DataTypes.INTEGER, 
+        allowNull: false 
+    },
+    correctAnswers: { 
+        type: DataTypes.INTEGER, 
+        allowNull: false 
+    },
+    testDate: { 
+        type: DataTypes.DATE, 
+        defaultValue: DataTypes.NOW 
+    }
+});
+
+class _SQLiteTestResultModel {
+    async create(testResult) {
+        return await TestResult.create(testResult);
+    }
+
+    async getTestResultsByUser(userId) {
+        return await TestResult.findAll({ 
+            where: { userId },
+            order: [['testDate', 'DESC']]
+        });
+    }
+}
+
+await sequelize.sync();
+const SQLiteTestResultModel = new _SQLiteTestResultModel();
+
+export { SQLiteTestResultModel, TestResult };
