1efa59c380e5cb9f6906500de3d764c1c3c340fa
sammywalden <swalden@umass.edu>
2024-12-10 20:43:52 -0500

    subject data backend

--- project/Back-end/controller/TaskController.js
+++ project/Back-end/controller/TaskController.js
@@ -109,6 +109,28 @@ export class TaskController {
         res.json({items});
     }
 
+
+    async getSetBySubject(req, res, sub) {
+      try {
+          const { setName, subjects, data } = req.body;
+          console.log(req.body);
+  
+          if (sub !== subjects) {
+              return res.status(400);
+          }
+          if (!setName || !subjects || !data) {
+              return res.status(500);
+          }
+          const item = await this.modelSets.create({ setName, subjects, data });
+  
+          return res.status(200)
+  
+      } catch (e) {
+          console.error(e);
+          return res.status(500);
+      }
+  }
+  
     async addSets(req, res){
         try {
           const { setName, subjects, data } = req.body;
